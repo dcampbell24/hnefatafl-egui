dea64e3626c5d3ee59fa134975b505f52cc0be83
David Campbell <dcampbell24@gmail.com>
2025-10-23 03:29:07 -0400

    Simplify self play.

--- src/bin/self-play.rs
+++ src/bin/self-play.rs
@@ -1,38 +1,28 @@
 use std::{error::Error, time::Duration};
 
 use hnefatafl::{
-    board::state::{BasicBoardState, BoardState},
+    board::state::BasicBoardState,
     game::GameStatus,
     pieces::Side,
     play::ValidPlay,
     preset::{boards, rules},
 };
-use hnefatafl_egui::ai::{Ai, BasicAi};
+use hnefatafl_egui::ai::Ai;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let game: hnefatafl::game::Game<BasicBoardState<u128>> =
+    let mut game: hnefatafl::game::Game<BasicBoardState<u128>> =
         hnefatafl::game::Game::new(rules::COPENHAGEN, boards::COPENHAGEN).unwrap();
 
     println!("{}", game.state.board);
 
-    let ai_attacker =
+    let mut ai_attacker =
         hnefatafl_egui::ai::BasicAi::new(game.logic, Side::Attacker, Duration::from_secs(15));
 
-    let ai_defender =
+    let mut ai_defender =
         hnefatafl_egui::ai::BasicAi::new(game.logic, Side::Defender, Duration::from_secs(15));
 
-    handle_messages(game, ai_attacker, ai_defender)?;
-
-    Ok(())
-}
-
-fn handle_messages<T: BoardState>(
-    mut game: hnefatafl::game::Game<T>,
-    mut ai_attacker: BasicAi<T>,
-    mut ai_defender: BasicAi<T>,
-) -> Result<(), Box<dyn Error>> {
     loop {
-        let (ValidPlay{ play}, info) = ai_attacker.next_play(&game.state).unwrap();
+        let (ValidPlay { play }, info) = ai_attacker.next_play(&game.state).unwrap();
         println!("play: {play}");
         println!("{info:?}\n");
 
@@ -44,7 +34,7 @@ fn handle_messages<T: BoardState>(
 
         println!("{}", game.state.board);
 
-        let (ValidPlay{ play}, info) = ai_defender.next_play(&game.state).unwrap();
+        let (ValidPlay { play }, info) = ai_defender.next_play(&game.state).unwrap();
         println!("play: {play}");
         println!("{info:?}\n");
 
